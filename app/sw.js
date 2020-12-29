6750ea8b5e90e8d0e43042e043e580ddbf421007
Neil Coutinho <njcoutinho@gmail.com>
2020-12-29 15:52:43 +0530

    Display notification on push event

--- app/sw.js
+++ app/sw.js
@@ -20,3 +20,21 @@
 /* eslint-env browser, serviceworker, es6 */
 
 'use strict';
+
+self.addEventListener('push', (event) => {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+
+    const title = 'Push Codelab';
+    const options = {
+        body: event.data.text(),
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+   
+
+
+    event.waitUntil(self.registration.showNotification(title, options));
+})
